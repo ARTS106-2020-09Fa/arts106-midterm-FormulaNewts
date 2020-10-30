bd583f17a3e131f716872543eeb33d2975b73b25
FormulaNewts <55401457+FormulaNewts@users.noreply.github.com>
2020-10-30 19:31:30 -0400

    FINAL (probably)
    if i keep staring at this nothing is going to get done so i'm submitting it now

---
+++ Assets/PlayerAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioMixerSnapshot idleSnapshot;
+    public AudioMixerSnapshot auxInSnapshot;
+
+
+    public AudioSource audioS;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("DangerousCabinet"))
+        {
+            auxInSnapshot.TransitionTo(0.5f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("DangerousCabinet"))
+        {
+            idleSnapshot.TransitionTo(0.5f);
+        }
+    }
+}
